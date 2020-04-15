7a5019211bfe90b656083c2f176986f1d24245a5
Owen Chanthala <owenchanthala@gmail.com>
2020-04-14 21:37:21 -0700

    heroku fix

---
+++ build/precache-manifest.9e155a6a2ce406ee7864fad77e73efa2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f79d6044a162315d6bcc36763112b320",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "97fa6cd8f3f47e5872d7",
+    "url": "/React-Employee-Directory/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "ea6852e8d60df988812b",
+    "url": "/React-Employee-Directory/static/css/main.d1814e84.chunk.css"
+  },
+  {
+    "revision": "97fa6cd8f3f47e5872d7",
+    "url": "/React-Employee-Directory/static/js/2.f85e2ff2.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/React-Employee-Directory/static/js/2.f85e2ff2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea6852e8d60df988812b",
+    "url": "/React-Employee-Directory/static/js/main.51baace0.chunk.js"
+  },
+  {
+    "revision": "707ae567f1cce12c286f",
+    "url": "/React-Employee-Directory/static/js/runtime-main.7b7e9a24.js"
+  }
+]);
